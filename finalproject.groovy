955c92badc062a6ded9a96a6cedc2419065d4388
tanviparab <tanviparab786@gmail.com>
2025-07-31 16:34:02 +0530

    frist step is added

--- finalproject.groovy
+++ finalproject.groovy
@@ -2,7 +2,9 @@ pipeline{
     agent any
     stages{
         stage('pulling code from repository'){
-            
+            steps{
+                git branch: 'main', url: 'https://github.com/tanviparab786/finalproject.code.git'
+            }
         }
     }
 }
