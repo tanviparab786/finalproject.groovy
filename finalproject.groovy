402f979d2f89eb6484c12f15cac441409180cc7f
tanviparab <tanviparab786@gmail.com>
2025-08-02 20:09:36 +0530

    fixed groovy syntax

--- finalproject.groovy
+++ finalproject.groovy
@@ -1,6 +1,13 @@
 pipeline {
     agent any
+
  stages {
+    stage('pulling code from repository'){
+        steps{
+               git branch: 'main', url: 'https://github.com/tanviparab786/finalproject.code.git'
+
+        }
+    }
         stage('sending code files') {
             steps {
                 sh ''' scp -o StrictHostKeyChecking=no -r * azureuser@20.193.250.218:/home/azureuser/'''
