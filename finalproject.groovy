f529b4aac83a4161da996399cd79c3fa2cf4a554
tanviparab <tanviparab786@gmail.com>
2025-08-02 19:55:49 +0530

    fixed groovy syntax

--- finalproject.groovy
+++ finalproject.groovy
@@ -10,18 +10,14 @@ pipeline {
 
         stage('sending code files') {
             steps {
-                sh '''
-                    scp -o StrictHostKeyChecking=no -r Dockerfile docker-compose.yml pom.xml src azureuser@20.193.250.218:/home/azureuser/
-                '''
+                sh '''scp -o StrictHostKeyChecking=no -r Dockerfile docker-compose.yml pom.xml src azureuser@20.193.250.218:/home/azureuser'''
             }
         }
 
         stage('sending template file') {
             steps {
                 withCredentials([sshUserPrivateKey(credentialsId: 'kubernetes-p1', keyFileVariable: 'KEY')]) {
-                    sh '''
-                        scp -o StrictHostKeyChecking=no -i $KEY -r templates azureuser@4.186.26.17:/home/azureuser/
-                    '''
+                    sh ''' scp -o StrictHostKeyChecking=no -i $KEY -r templates azureuser@4.186.26.17:/home/azureuser/'''
                 }
             }
         }
