ead5c48bf47ceaaed6b3dcf31619f770711fff04
tanviparab <tanviparab786@gmail.com>
2025-08-05 16:45:01 +0530

    fixed groovy syntax

--- finalproject.groovy
+++ finalproject.groovy
@@ -27,9 +27,12 @@ pipeline {
         steps{
                sshagent(['docker1']) {
               
-            withCredentials([string(credentialsId: 'dockerimage', variable: 'docker_password')]) {
+            withCredentials([string(credentialsId: 'dockerimage', variable: 'DOCKER_PASSWORD')]) {
                 sh 'hostname'
-                sh ''' ssh -o StrictHostKeyChecking=no azureuser@4.240.96.242 echo "$docker_password" | docker login -u "tanvi2828" --password-stdin'''
+                sh '''ssh -o StrictHostKeyChecking=no azureuser@4.240.96.242 bash -c \'
+                 echo "$DOCKER_PASSWORD" | docker login -u "tanvi2828" --password-stdin
+                    \'
+                    '''
                 sh '''ssh -o StrictHostKeyChecking=no azureuser@4.240.96.242 "docker image push tanvi2828/f1f8"'''                                                        
                  //    sh docker image push tanvi2828/$JOB_NAME:v1.$BUILD_ID
                 //   sh docker image push tanvi2828/$JOB_NAME:latest
