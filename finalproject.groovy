39e5db760f236752d5b1fc7e5c1b49804cd4648e
tanviparab <tanviparab786@gmail.com>
2025-08-07 16:13:37 +0530

    change in groovy

--- finalproject.groovy
+++ finalproject.groovy
@@ -13,9 +13,10 @@ pipeline {
         }
         stage('sending template file') {
             steps {
-                // sshagent(['kubernetes-project']) {
+                 sshagent(['kubernetes']) {
                 sh '''scp -o StrictHostKeyChecking=no -i ~/.ssh/id_k8s -r templates azureuser@4.186.26.17:/home/azureuser/'''
-                //    }
+                sh ''' ssh -o StrictHostKeyChecking=no azureuser@4.186.26.17 'rm -r ~/templates' '''
+                   }
             }
         }
         stage('Build and push docker image') {
